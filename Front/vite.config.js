6310d0f633f3ce360963b3f124420c1f49f8a7de
vansh167 <vanshdoc7gmail.com>
2025-11-07 12:14:37 +0530

    chsnges

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -4,4 +4,31 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  build: {
+    outDir: "dist",
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1600,
+    assetsInlineLimit: 4096,
+    sourcemap: false,
+    minify: 'esbuild',
+    esbuild: {
+      drop: ['console', 'debugger'],
+    },
+  },
+
+
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
+  },
+});
