ef3f970a04bdc0c5c712b4ad32aa0f72681e957c
Vansh <vanshdoc7@gmail.com>
2025-11-25 21:26:09 +0530

    first

--- Front/src/feature/Feature.jsx
+++ Front/src/feature/Feature.jsx
@@ -1,35 +1,34 @@
-import React from 'react'
-import './feature.css' // optional if you style separately
+import React from "react";
+import "./feature.css";
+
+// Import images from src
+import f1 from "../assets/features/f1.png";
+import f2 from "../assets/features/f2.png";
+import f3 from "../assets/features/f3.png";
+import f4 from "../assets/features/f4.png";
+import f5 from "../assets/features/f5.png";
+import f6 from "../assets/features/f6.png";
 
 const Feature = () => {
+  const features = [
+    { img: f1, title: "Free Shipping" },
+    { img: f2, title: "Online Order" },
+    { img: f3, title: "Save Money" },
+    { img: f4, title: "Offers" },
+    { img: f5, title: "Promotions" },
+    { img: f6, title: "Happy Sell" },
+  ];
+
   return (
     <section id="feature" className="section-p1">
-      <div className="fe-box">
-        <img src="./src//assets/features/f1.png" alt="Free Shipping" />
-        <h6>Free Shipping</h6>
-      </div>
-      <div className="fe-box">
-        <img src="./src//assets/features/f2.png" alt="Online Order" />
-        <h6>Online Order</h6>
-      </div>
-      <div className="fe-box">
-        <img src="./src//assets/features/f3.png" alt="Save Money" />
-        <h6>Save Money</h6>
-      </div>
-      <div className="fe-box">
-        <img src="./src//assets/features/f4.png" alt="Offers" />
-        <h6>Offers</h6>
-      </div>
-      <div className="fe-box">
-        <img src="./src//assets/features/f5.png" alt="Promotions" />
-        <h6>Promotions</h6>
-      </div>
-      <div className="fe-box">
-        <img src="./src//assets/features/f6.png" alt="Happy Sell" />
-        <h6>Happy Sell</h6>
-      </div>
+      {features.map((feature, index) => (
+        <div className="fe-box" key={index}>
+          <img src={feature.img} alt={feature.title} />
+          <h6>{feature.title}</h6>
+        </div>
+      ))}
     </section>
-  )
-}
+  );
+};
 
 export default Feature;
