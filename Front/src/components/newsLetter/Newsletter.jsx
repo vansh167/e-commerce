a4521010a94a56ca4fed038a42c3f4475a6eb170
Vansh <vanshdoc7@gmail.com>
2025-11-26 01:19:03 +0530

    1

--- Front/src/components/newsLetter/Newsletter.jsx
+++ Front/src/components/newsLetter/Newsletter.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import './Newsletter.css'
+import React from "react";
+import "./Newsletter.css";
+
 const Newsletter = () => {
   return (
-    <>
-      <div className="newsletter">
-        <h1>Get Exclusive Offers On Your Email</h1>
-        <p>Subscribe to our newsletter and stay updated</p>
-        <div>
-          <input type="email" placeholder='Your Email id' />
-          <button>Subscribe</button>
-        </div>
-      </div>
-    </>
-  )
-}
+    <section className="newsletter">
+      <h1>Get Exclusive Offers On Your Email</h1>
+      <p>Subscribe to our newsletter and stay updated</p>
 
-export default Newsletter
+      <div className="newsletter__form">
+        <input type="email" placeholder="Your Email id" />
+        <button>Subscribe</button>
+      </div>
+    </section>
+  );
+};
+
+export default Newsletter;
